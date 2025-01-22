8a0f65d96e6121de223ad844fd246582fc81910f
Alana Barrett-Frew <alanabarrett-frew@hotmail.com>
2025-01-22 18:23:44 +0000

    trying an alternative way to configure the api key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,4 @@
-declare global {
-  interface Window {
-    process: {
-      env: {
-        [key: string]: string;
-      };
-    }
-  }
-}
-
 export const environment = {
   production: true,
-  apiKey: window.process?.env?.['WEATHER_API_KEY'] || ''
+  apiKey: process.env['WEATHER_API_KEY']
 };
