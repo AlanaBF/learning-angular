bb24759113c3672b1b7d883b47dd3c593ce49be0
Alana Barrett-Frew <alanabarrett-frew@hotmail.com>
2025-01-22 21:20:31 +0000

    still trying to get api key to work

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,12 @@
+declare global {
+  interface Window {
+    __env: {
+      WEATHER_API_KEY: string;
+    }
+  }
+}
+
 export const environment = {
   production: true,
-  apiKey: '',
+  apiKey: window.__env?.WEATHER_API_KEY || ''
 };
