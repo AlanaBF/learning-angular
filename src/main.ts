bc6b5768d34617ca2e36513ee7dede1b74ddb332
Alana Barrett-Frew <alanabarrett-frew@hotmail.com>
2025-01-22 18:15:24 +0000

    still trying to get api key to configure

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,12 @@ import { provideRouter } from '@angular/router';
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
 import { provideHttpClient } from '@angular/common/http';
-
+import { environment } from './environments/environment.prod';
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
     provideHttpClient(),
+    { provide: 'API_KEY', useValue: environment.apiKey }
   ],
 }).catch((err) => console.error(err));
 
