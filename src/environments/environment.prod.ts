17d162ad57283078405503e1a455447dbcf0810c
Alana Barrett-Frew <alanabarrett-frew@hotmail.com>
2025-01-22 18:36:59 +0000

    another try to connect api key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,12 @@
+declare global {
+  interface Window {
+    env: {
+      WEATHER_API_KEY: string;
+    }
+  }
+}
+
 export const environment = {
   production: true,
-  apiKey: process.env['WEATHER_API_KEY']
+  apiKey: window?.env?.WEATHER_API_KEY || ''
 };
